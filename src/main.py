af538766b7be82ad931639e8f92760b4521487cd
CodingMan <truth.tseng@ailabs.tw>
2021-06-19 16:30:21 +0800

    Add show state

--- src/main.py
+++ src/main.py
@@ -13,7 +13,6 @@ class Breaker:
         self.driver = webdriver.Chrome('chromedriver')
         self.driver.set_page_load_timeout(100)
         self.driver.delete_all_cookies()
-
         self.wait = WebDriverWait(self.driver, 10)
 
     def press_button(self, func_name: str):
@@ -23,15 +22,33 @@ class Breaker:
         # self.driver.find_element(By.CSS_SELECTOR, ".e2e__up").click()
 
         func_name_list = [
-            'start', 'left', 'down', 'right', 'up'
+            'start', 'left', 'down', 'right', 'up', 'back'
         ]
         func_name = func_name.lower()
         if func_name not in func_name_list:
             raise ValueError
+        if func_name == 'back':
+            self.driver.find_element(By.CSS_SELECTOR, "div:nth-child(1) > .game__button:nth-child(1)").click()
         if func_name == 'start':
             self.driver.find_element(By.CSS_SELECTOR, f".e2e__{func_name}-button").click()
         else:
             self.driver.find_element(By.CSS_SELECTOR, f".e2e__{func_name}").click()
+
+    def show(self):
+        elements = self.driver.find_elements_by_xpath("//td[contains(@class, 'well__cell')]")
+
+        for i in range(len(elements)):
+            current_state = elements[i].get_attribute('class')
+            if current_state == 'well__cell':
+                print('  ', end='')
+            elif current_state == 'well__cell well__cell--live':
+                print('▇▇', end='')
+            elif current_state == 'well__cell well__cell--bar':
+                # print('ㄧ', end='')
+                pass
+            if i % 10 == 9:
+                print('')
+        print('-----')
 
     def count(self):
 
@@ -42,15 +59,10 @@ class Breaker:
             ))
         self.logger.info('Load', 'success')
         self.press_button('start')
-        time.sleep(1)
-        elements = self.driver.find_elements_by_xpath("//div[@class='game__topleft']")
-        for e in elements:
-            print(e.get_attribute('id'))
+        self.show()
+
         self.press_button('left')
-        time.sleep(1)
-        elements = self.driver.find_elements_by_xpath("//div[@class='game__topleft']")
-        for e in elements:
-            print(e.get_attribute('id'))
+        self.show()
         try:
             pass
         finally:
